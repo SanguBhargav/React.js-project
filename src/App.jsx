f6c90cad9489f34f77f72bc1080daad5696e42c9
SanguBhargav <bhargavsangu2@gmail.com>
2026-02-07 11:30:43 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,61 @@
 import React from 'react'
-import Greeting from './Greeting.jsx'//
- import Conditionalrendering from './component/Conditionalrendering.jsx'//
-import Student from './component/Student.jsx'//
-import Setcount from './component/Setcount.jsx'//
-import Ternary from './component/Ternary.jsx'//
-import Stu from './component/Stu.jsx'//
-import Course from './component/Course.jsx'//
+import Greeting from './Greeting.jsx'
+ import Conditionalrendering from './component/Conditionalrendering.jsx'
+import Student from './component/Student.jsx'
+import Setcount from './component/Setcount.jsx'
+import Ternary from './component/Ternary.jsx'
+import Stu from './component/Stu.jsx'
+import Course from './component/Course.jsx'
 import Fruits from './component/Fruits.jsx'
-import Even from './component/Even.jsx'//
-import Marks from './component/Marks.jsx'//
-import Studen from './component/Studen.jsx'//
-import Form from './component/Form.jsx'//
-import New from './component/New.jsx'//
-import Sd from './component/Sd.jsx'//
-import "./component/Helio.css"//
-import Watch from './component/Watch.jsx'//
-import "./component/Timer.css"//
-import UseEffectApi from './component/UseEffectApi.jsx'//
-import UseRef from './component/UseRef.jsx'//
-import For from './component/For.jsx'//
-import Use from './component/Use.jsx'//
-import { BrowserRouter,Routes,Route,Link} from 'react-router-dom'//
-import Home from "./Router/Home"//
-import About from "./Router/About"//
-import Services from "./Router/Services"//
-import Contact from "./Router/Contact"//
-import AppDevelopment from "./Router/AppDevelopment.jsx"//
-import WebDevelopment from "./Router/WebDevelopment.jsx"//
-import UseCall from './Router/UseCall.jsx'//
-import UseCallBackList from './Router/UseCallBackList.jsx'//
-import MiniChat from './Router/MiniChat.jsx'//
-import Exam from './Practice/Exam.jsx'//
-import Like from './Practice/Like.jsx'//
-import List from './Router/List.jsx'//
-import Blog from './Router/Blog.jsx'//
-import ComponentA from './Practice/ComponentA.jsx'//
-import Effec from './Practice/Effec.jsx'//
-import TicTacToe from './Router/TicTacToe.jsx'//
-import Ref from './Practice/Ref.jsx'//
-import Re from './Practice/Re.jsx'//
-import Call from './Practice/Call.jsx'//
+import Even from './component/Even.jsx'
+import Marks from './component/Marks.jsx'
+import Studen from './component/Studen.jsx'
+import Form from './component/Form.jsx'
+import New from './component/New.jsx'
+import Sd from './component/Sd.jsx'
+import "./component/Helio.css"
+import Watch from './component/Watch.jsx'
+import "./component/Timer.css"
+import UseEffectApi from './component/UseEffectApi.jsx'
+import UseRef from './component/UseRef.jsx'
+import For from './component/For.jsx'
+import Use from './component/Use.jsx'
+import { BrowserRouter,Routes,Route,Link} from 'react-router-dom'
+import Home from "./Router/Home"
+import About from "./Router/About"
+import Services from "./Router/Services"
+import Contact from "./Router/Contact"
+import AppDevelopment from "./Router/AppDevelopment.jsx"
+import WebDevelopment from "./Router/WebDevelopment.jsx"
+import UseCall from './Router/UseCall.jsx'
+import UseCallBackList from './Router/UseCallBackList.jsx'
+import MiniChat from './Router/MiniChat.jsx'
+import Exam from './Practice/Exam.jsx'
+import Like from './Practice/Like.jsx'
+import List from './Router/List.jsx'
+import Blog from './Router/Blog.jsx'
+import ComponentA from './Practice/ComponentA.jsx'
+import Effec from './Practice/Effec.jsx'
+import TicTacToe from './Router/TicTacToe.jsx'
+import Ref from './Practice/Ref.jsx'
+import Re from './Practice/Re.jsx'
+import Call from './Practice/Call.jsx'
 
-import Mygoal from './ApInt/Mygoal.jsx'//
-import Reff from './Practice/Reff.jsx'//
-import Back from './Practice/Back.jsx'//
+import Mygoal from './ApInt/Mygoal.jsx'
+import Reff from './Practice/Reff.jsx'
+import Back from './Practice/Back.jsx'
 import Apple from './ApInt/Apple.jsx'
 import HideOutput from './component/HideOutput.jsx'
 export default function App() {
   
-/* const students=["helio","hlo","hi"]*/
+const students=["helio","hlo","hi"]
   
 
   return (
     <div>
-      {/*<h1>Student Details</h1>*/}
+      <h1>Student Details</h1>
 
-     {/*<Student
+     <Student
         name="Swami"
         course="CSE"
         age={19}
@@ -136,11 +136,11 @@ export default function App() {
         email="swami@gmail.com"
         year={2026}
       />
- {/*<Watch/> */} {/*<UseEffectApi/>*/} {/*<UseRef/>*/} {/*<For /> */}{/*<Use/>*/} {/*<MiniChat/>*/}
-{/*<div className="hello">*/}
+ <Watch/>  <UseEffectApi/> <UseRef/> <For /> <Use/> <MiniChat/>
+<div className="hello">
   
 
-{/*<BrowserRouter>
+<BrowserRouter>
 
 <Link to={"/Home"}>Home</Link>
   <Link to={"/About"}>About</Link>
@@ -156,9 +156,9 @@ export default function App() {
   <Route path="/list" element={<List />} />
  <Route path="/Blogs/:id" element={<Blog />} />
 </Routes>
-</BrowserRouter>*/}  {/*<UseCall/> <Exam/> <Like/> <ComponentA/> <Effec/> <TicTacToe /> <Ref/> <Re/> <Call/>*/} <Apple/> {/*<Mygoal/> <Reff/> <Back/> <HideOutput/>*/}
+</BrowserRouter>*/}  <UseCall/> <Exam/> <Like/> <ComponentA/> <Effec/> <TicTacToe /> <Ref/> <Re/> <Call/><Apple/> <Mygoal/> <Reff/> <Back/> <HideOutput/>
 
-{/*</div>*/}
+</div>
 
 
     </div>
